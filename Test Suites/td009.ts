219309b0ab4c141d7cfbff8475ee3fe1b504a7bf
huy <huynguyen2601@gmail.com>
2023-07-28 11:07:18 +0700

    add

---
+++ Test Suites/td009.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>td009</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a229ea08-6c77-475f-9c99-15984be66ded</testSuiteGuid>
+   <testCaseLink>
+      <guid>d2ed4744-f59a-4e68-bd83-c693dbbd82c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/P4_CreateCustomerEnter5Times</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
