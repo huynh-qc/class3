4f9f1fed57c791e1e95f4397655fb233e91521e7
huy <huynguyen2601@gmail.com>
2023-07-28 11:03:09 +0700

    suite

--- Test Suites/5times.ts
+++ Test Suites/5times.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>365ea06c-9bd6-4c00-ac1a-a8e8d54ca3b6</testSuiteGuid>
+   <testCaseLink>
+      <guid>eb1c9b03-a647-4f1f-b0a4-3e4380831cec</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/P4_CreateCustomerEnter5Times</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
