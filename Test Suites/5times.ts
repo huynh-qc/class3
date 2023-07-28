d5cae8f81c795fb52bbc4cd5a3a0edb110da567b
huy <huynguyen2601@gmail.com>
2023-07-28 11:02:54 +0700

    stuite

---
+++ Test Suites/5times.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>5times</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>365ea06c-9bd6-4c00-ac1a-a8e8d54ca3b6</testSuiteGuid>
+</TestSuiteEntity>
