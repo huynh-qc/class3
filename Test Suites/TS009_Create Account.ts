45d4ffc9e2b845a2c4462d4b48b06afd225569b9
huy <huynguyen2601@gmail.com>
2023-07-28 16:15:48 +0700

    update testsuite and testsuite collection

---
+++ Test Suites/TS009_Create Account.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS009_Create Account</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1cc6ac7e-6dbf-4e8f-95c7-3b2c479321ce</testSuiteGuid>
+   <testCaseLink>
+      <guid>f036091b-2e2a-481a-8798-ba6da8aa6995</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/P4_CreateCustomerEnter5Times</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
